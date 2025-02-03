10a63d76e63dfe8efd5b94e92b67e63e8c509b9e
Mauricio <mgp.94@hotmail.com>
2025-02-03 14:12:25 -0500

    "vemos el uso de useRef()"

--- 05-react-buscador-peliculas/src/App.jsx
+++ 05-react-buscador-peliculas/src/App.jsx
@@ -1,24 +1,65 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 import { Movies } from './components/Movies'
 import { useMovies } from './hooks/useMovies'
 
+
+function useSearch() {
+  const [search, updateSearch] = useState('')
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+
+    if (search === '') {
+      setError('No se puede buscar una película vacía')
+      return
+    }
+
+    if (search.match(/^\d+$/)) {
+      setError('No se puede buscar una película con un número')
+      return
+    }
+
+    if (search.length < 3) {
+      setError('La búsqueda debe tener al menos 3 caracteres')
+      return
+    }
+
+    setError(null)
+  }, [search])
+  return { search, updateSearch, error }
+
+}
+
 function App() {
 
   const { movies: mappeMovies } = useMovies()
+  const { search, updateSearch, error } = useSearch()
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log({ search })
+  }
+  const handleChange = (event) => {
+    updateSearch(event.target.value)
+  }
+
 
   return (
     <div className='page'>
       <header>
         <h1>Buscador de peliculas </h1>
-        <form className='form'>
-          <input placeholder='Avegers,Star wars, The Matrix ...' />
+        <form className='form' onSubmit={handleSubmit}>
+          <input onChange={handleChange} value={search} name='query' placeholder='Avegers,Star wars, The Matrix ...' />
           <button type='submit'>Buscar </button>
         </form>
+        {error && <p style={{ color: 'red' }}>{error}</p>}
+
       </header>
       <main>
         <Movies movies={mappeMovies} />
       </main>
-    </div>
+    </div >
   )
 }
 
